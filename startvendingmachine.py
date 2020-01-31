14cb228bc84405e41b5504e1e347ceec79f02f5d
HappylabWien <wien@happylab.at>
2020-01-31 08:35:37 +0000

    add file startvendingmachine.py

---
+++ startvendingmachine.py
@@ -0,0 +1,13 @@
+﻿#!/usr/bin/python3
+
+from raspifabman import FabmanBridge
+from vendingmachine import VendingMachine
+import sys # because api token is read from command line
+
+config = { # change default settings
+	"api_url_base"       : "https://internal.fabman.io/api/v1/", # api url base / for production systems remove "internal."
+	"heartbeat_interval" : 30
+}
+bridge = FabmanBridge(sys.argv[1], config)
+vending_machine = VendingMachine(bridge) # read config from "config.json"
+vending_machine.run()
