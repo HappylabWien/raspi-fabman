7b7a214bb582ac88efadc0a413fac4627d260ec3
HappylabWien <wien@happylab.at>
2022-02-24 11:35:08 +0000

    watchdog added

---
+++ watchdog.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# reboots raspberry, when www.fabman.io is not reachable
+# checked via crontab
+# reboots logged in /home/pi/raspi-fabman/watchdog.log
+# source: https://weworkweplay.com/play/rebooting-the-raspberry-pi-when-it-loses-wireless-connection-wifi/
+
+ping -c4 www.fabman.io > /dev/null
+ 
+if [ $? != 0 ] 
+then
+  echo "Reboot at $(/bin/date)" >> /home/pi/raspi-fabman/watchdog.log
+  sudo /sbin/shutdown -r now
+fi
