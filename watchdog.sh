2600652b9a03a53ad1db49c8b80a529a754d1145
HappylabWien <wien@happylab.at>
2022-09-07 16:15:07 +0100

    watchdog reactivated

--- watchdog.sh
+++ watchdog.sh
@@ -19,7 +19,7 @@ fi
 if [ $? != 0 ]
 then
   echo "$(/bin/date) MICROPOS NOT RUNNING -> STARTING MICROPOS" >> /home/pi/raspi-fabman/watchdog.log
-  ############runuser -l pi -c "cd /home/pi/raspi-fabman;/usr/bin/python3 /home/pi/raspi-fabman/micropos.py >> /home/pi/raspi-fabman/log/micropos.log 2>&1 &"
+  runuser -l pi -c "cd /home/pi/raspi-fabman;/usr/bin/python3 /home/pi/raspi-fabman/micropos.py >> /home/pi/raspi-fabman/log/micropos.log 2>&1 &"
   #sudo /sbin/shutdown -r now
 #else
   #echo "$(/bin/date) MICROPOS OK" >> /home/pi/raspi-fabman/watchdog.log
